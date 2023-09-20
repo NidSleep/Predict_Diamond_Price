e7ec49951300aa01513e5052f8c9aa20cc0450bc
NidSleep <103735341+NidSleep@users.noreply.github.com>
2023-09-21 01:18:09 +0800

    Update predictDiamondPrice.py

--- predictDiamondPrice.py
+++ predictDiamondPrice.py
@@ -60,8 +60,8 @@ if st.button("Predict Diamond Price"):
         # Calculate lower and upper bounds of the prediction interval
         lower_bound = predicted_price - margin_of_error
         upper_bound = predicted_price + margin_of_error
-        st.success(f"Predicted Price: ${predicted_price:.2f}\n (The result may fall within this range: ${lower_bound:.2f} to ${upper_bound:.2f})")
-        st.write(f"The result may fall within this range: ${lower_bound:.2f} ~ ${upper_bound:.2f}")
+        st.success(f"Predicted Price: ${predicted_price:.2f}")
+        st.write("The result may fall within this range: ${lower_bound:.2f} - ${upper_bound:.2f}")
 
     else:
         st.error("An error occurred while making predictions.")
