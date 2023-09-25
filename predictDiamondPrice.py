d96b6e6bc5129dbe90852f36f28e2cde0496e01d
NidSleep <103735341+NidSleep@users.noreply.github.com>
2023-09-26 04:52:07 +0800

    Update predictDiamondPrice.py

--- predictDiamondPrice.py
+++ predictDiamondPrice.py
@@ -6,7 +6,7 @@ import base64
 from scipy.stats import norm
 
 # Load your trained scikit-learn KNeighborsRegressor model here
-model = joblib.load('model.pkl')
+model = joblib.load('model2.pkl')
 
 # Dictionary to encode color and clarity
 color_encoding = {"J": 1, "I": 2, "H": 3, "G": 4, "F": 5, "E": 6, "D": 7}
@@ -26,9 +26,9 @@ def encode_clarity(clarity):
 def predict_price(carat, color_encoded, clarity_encoded, depth, table, x, y, z):
     try:
         # Make price predictions using the model
-        input_data = np.array([carat, color_encoded, clarity_encoded, x, y, z]).reshape(1, -1)        
+        input_data = np.array([carat, color_encoded, clarity_encoded, x, y, z]).reshape(1, -1)
         predicted_price = model.predict(input_data)
-        
+
         return predicted_price[0]
     except Exception as e:
         return None
@@ -61,7 +61,7 @@ if st.button("Predict Diamond Price"):
         lower_bound = predicted_price - margin_of_error
         upper_bound = predicted_price + margin_of_error
         st.success(f"Predicted Price: ${predicted_price:.2f}")
-        st.write(f" +- ${margin_of_error:.2f}")
+        #st.write(f" ± ${margin_of_error:.2f}")
 
     else:
         st.error("An error occurred while making predictions.")
